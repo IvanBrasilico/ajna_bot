bc08c3bd663e42ef2f3f26dead07fbbcf3d1cb8a
IvanBrasilico <brasilico.ivan@gmail.com>
2020-06-10 13:42:13 -0300

    Consulta de Fichas

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 import logging
 import sys
+
 sys.path.append('.')
+from datetime import datetime, timedelta
+
+from pymongo import MongoClient
+
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 
 from bhadrasana.models import db_session
-from bhadrasana.models.ovrmanager import get_ovr_responsavel
+from bhadrasana.models.ovrmanager import get_ovr_responsavel, get_ovr_container
+from bhadrasana.models.rvfmanager import get_rvfs_filtro
+from bhadrasana.models.virasana_manager import get_dues_container, get_detalhes_mercante
+
 from config import BOTTOKEN
+
+from ajna_commons.flask.conf import DATABASE, MONGODB_URI
+
+conn = MongoClient(host=MONGODB_URI)
+mongodb = conn[DATABASE]
 
 print(BOTTOKEN)
 
@@ -23,10 +36,32 @@ def start(update, context):
 
 
 def container(update, context):
+    fim = datetime.now()
+    inicio = fim - timedelta(days=90)
     numero = context.args[0]
-    print(context.args, type(context.args))
-    logger.info(numero)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=numero)
+    logger.info('Consultando contêiner %s' % numero)
+    logger.info('get_rvfs_filtro')
+    rvfs = get_rvfs_filtro(db_session, {'numerolote': numero,
+                                        'datainicio': inicio,
+                                        'datafim': fim})
+    logger.info('get_dues_container')
+    dues = get_dues_container(mongodb, numero)
+    lista_numeroDUEs = [due['numero'] for due in dues]
+    logger.info('get_ovr_container')
+    ces, ovrs = get_ovr_container(db_session, numero, inicio, fim,
+                                  lista_numeroDUEs)
+    logger.info('get detalhes CE Mercante')
+    infoces = get_detalhes_mercante(db_session, ces)
+    result = []
+    result.append('Fichas')
+    result.append(' - '.join([str(ovr.id) for ovr in ovrs]))
+    result.append('Verificações Físicas')
+    result.append(' - '.join([str(rvf.id) for rvf in rvfs]))
+    result.append('DUEs')
+    result.append(' - '.join(lista_numeroDUEs))
+    result.append(str(infoces))
+    result = '\n'.join(result)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 
 def minhas_fichas(update, context):
