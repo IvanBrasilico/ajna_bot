5fb36a9d2539da95618c86e01a3f417c9563e5b2
IvanBrasilico <Iv@n1234>
2020-06-10 17:11:33 -0300

    Consulta de fotos e de escaneamentos

--- app/main.py
+++ app/main.py
@@ -1,25 +1,16 @@
+import io
 import logging
 import sys
+
+import requests
 
 sys.path.append('.')
 from datetime import datetime, timedelta
 
-from pymongo import MongoClient
-
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 
-from bhadrasana.models import db_session
-from bhadrasana.models.ovrmanager import get_ovr_responsavel, get_ovr_container
-from bhadrasana.models.rvfmanager import get_rvfs_filtro
-from bhadrasana.models.virasana_manager import get_dues_container, get_detalhes_mercante
-
-from config import BOTTOKEN
-
-from ajna_commons.flask.conf import DATABASE, MONGODB_URI
-
-conn = MongoClient(host=MONGODB_URI)
-mongodb = conn[DATABASE]
+from config import BOTTOKEN, APIURL
 
 print(BOTTOKEN)
 
@@ -32,55 +23,84 @@ logger = logging.getLogger()
 
 def start(update, context):
     logger.info('start')
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    result = '''
+    Bem vindo ao cliente Telegram do AJNA (alfa)
+    Comandos disponíveis:
+    /fichas <cpf> - lista fichas atribuídas para o CPF
+    /conteiner <numero> - informações para o contêiner <numero>
+    /scan <numero> - escaneamentos para o contêiner <numero>
+    /fotos <id> - fotos de verificação para a ficha <id>
+    /upload_ficha <id> <imagem> - Upload de imagem na ficha <id> 
+    /upload <numero> <imagem> - Upload de imagem na ficha do contêiner <numero> (Se houver para o Usuário) 
+    '''
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 
-def container(update, context):
+def conteiner(update, context):
     fim = datetime.now()
     inicio = fim - timedelta(days=90)
     numero = context.args[0]
     logger.info('Consultando contêiner %s' % numero)
-    logger.info('get_rvfs_filtro')
-    rvfs = get_rvfs_filtro(db_session, {'numerolote': numero,
-                                        'datainicio': inicio,
-                                        'datafim': fim})
-    logger.info('get_dues_container')
-    dues = get_dues_container(mongodb, numero)
-    lista_numeroDUEs = [due['numero'] for due in dues]
-    logger.info('get_ovr_container')
-    ces, ovrs = get_ovr_container(db_session, numero, inicio, fim,
-                                  lista_numeroDUEs)
-    logger.info('get detalhes CE Mercante')
-    infoces = get_detalhes_mercante(db_session, ces)
-    result = []
-    result.append('Fichas')
-    result.append(' - '.join([str(ovr.id) for ovr in ovrs]))
-    result.append('Verificações Físicas')
-    result.append(' - '.join([str(rvf.id) for rvf in rvfs]))
-    result.append('DUEs')
-    result.append(' - '.join(lista_numeroDUEs))
-    result.append(str(infoces))
-    result = '\n'.join(result)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+    payload = {'numerolote': numero,
+               'datainicio': inicio,
+               'datafim': fim}
+    r = requests.post(APIURL + 'consulta_container_text', data=payload)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=r.text)
+
+
+def conteiner2(update, context):
+    fim = datetime.now()
+    inicio = fim - timedelta(days=90)
+    numero = context.args[0]
+    logger.info('Consultando contêiner %s' % numero)
+    payload = {'numerolote': numero,
+               'datainicio': inicio,
+               'datafim': fim}
+    r = requests.post(APIURL + 'consulta_container', data=payload)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=r.text)
 
 
 def minhas_fichas(update, context):
     cpf = context.args[0]
     logger.info(cpf)
-    ovrs = get_ovr_responsavel(db_session, cpf)
-    logger.info(len(ovrs))
-    if len(ovrs) == 0:
-        result = 'Sem Fichas atribuídas para o Usuário {}'.format(cpf)
-    else:
-        result = '\n'.join([str(ovr.id) + ' - ' + ovr.numeroCEmercante for ovr in ovrs])
-    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+    r = requests.get(APIURL + 'minhas_fichas_text?cpf=%s' % cpf)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=r.text)
+
+
+def send_scan(update, context):
+    numero = context.args[0]
+    logger.info('Consultando contêiner %s' % numero)
+    r = requests.get(APIURL + 'escaneamentos_container/%s' % numero)
+    logger.info(r.text)
+    imagens = r.json()
+    for _id in imagens:
+        r = requests.get(APIURL + 'scan/%s' % _id)
+        bio = io.BytesIO(r.content)
+        bio.name = '%s.jpeg' % _id
+        bio.seek(0)
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=bio)
+
+
+def send_fotos(update, context):
+    rvf_id = context.args[0]
+    logger.info('Consultando rvf %s' % rvf_id)
+    r = requests.get(APIURL + 'imagens_rvf/%s' % rvf_id)
+    logger.info(r.text)
+    imagens = r.json()
+    for _id in imagens:
+        r = requests.get(APIURL + 'image/%s' % _id)
+        bio = io.BytesIO(r.content)
+        bio.name = '%s.jpeg' % _id
+        bio.seek(0)
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=bio)
 
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-caps_handler = CommandHandler('container', container)
-dispatcher.add_handler(caps_handler)
-caps_handler = CommandHandler('fichas', minhas_fichas)
-dispatcher.add_handler(caps_handler)
+dispatcher.add_handler(CommandHandler('conteiner', conteiner))
+dispatcher.add_handler(CommandHandler('conteiner2', conteiner2))
+dispatcher.add_handler(CommandHandler('fichas', minhas_fichas))
+dispatcher.add_handler(CommandHandler('scan', send_scan))
+dispatcher.add_handler(CommandHandler('fotos', send_fotos))
 
 updater.start_polling()
