f5698d2ec734bccad9729cddc99dd4369d076dc5
IvanBrasilico <brasilico.ivan@gmail.com>
2020-06-10 12:01:48 -0300

    Consulta de Fichas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 import logging
-
+import sys
+sys.path.append('.')
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
+
+from bhadrasana.models import db_session
+from bhadrasana.models.ovrmanager import get_ovr_responsavel
 from config import BOTTOKEN
+
 print(BOTTOKEN)
 
 updater = Updater(token=BOTTOKEN, use_context=True)
@@ -24,9 +29,23 @@ def container(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=numero)
 
 
+def minhas_fichas(update, context):
+    cpf = context.args[0]
+    logger.info(cpf)
+    ovrs = get_ovr_responsavel(db_session, cpf)
+    logger.info(len(ovrs))
+    if len(ovrs) == 0:
+        result = 'Sem Fichas atribuídas para o Usuário {}'.format(cpf)
+    else:
+        result = '\n'.join([str(ovr.id) + ' - ' + ovr.numeroCEmercante for ovr in ovrs])
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 caps_handler = CommandHandler('container', container)
+dispatcher.add_handler(caps_handler)
+caps_handler = CommandHandler('fichas', minhas_fichas)
 dispatcher.add_handler(caps_handler)
 
 updater.start_polling()
