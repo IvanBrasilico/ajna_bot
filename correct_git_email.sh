242101bbe957ebf1084f6f4280e37b4cadb86834
IvanBrasilico <brasilico.ivan@gmail.com>
2020-06-18 22:49:30 -0300

    Correct git email - push bot - mods isbn

---
+++ correct_git_email.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="iv@n1234"
+CORRECT_NAME="IvanBrasilico"
+CORRECT_EMAIL="brasilico.ivan@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
