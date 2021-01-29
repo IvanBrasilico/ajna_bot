92873a4aaff3ac4d2211bf92bf738fc942a3ec73
IvanBrasilico <brasilico.ivan@gmail.com>
2021-01-29 08:05:46 -0300

    --

--- correct_git_email.sh
+++ correct_git_email.sh
@@ -2,9 +2,9 @@
 
 git filter-branch --env-filter '
 
-OLD_EMAIL="Iv@n1234"
-CORRECT_NAME="IvanBrasilico"
-CORRECT_EMAIL="brasilico.ivan@gmail.com"
+OLD_EMAIL="xyGxo8-nezjox-rakjeb"
+CORRECT_NAME="tiberiodamaso"
+CORRECT_EMAIL="tiberio.damaso@icloud.com"
 
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
